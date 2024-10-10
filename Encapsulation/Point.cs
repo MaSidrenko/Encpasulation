807643e22aa50251b59140a902af8fce3cdbaa92
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-10-10 20:18:31 +0300

    Operator overloading DONE

--- Encapsulation/Point.cs
+++ Encapsulation/Point.cs
@@ -54,14 +54,64 @@ namespace Encapsulation
 			this.y = y;
 		}
 		public double Distance(Point other)
+		{			
+			double distance = Math.Sqrt(Math.Pow(this.x - other.x, 2) + Math.Pow(this.y - other.y,2));
+			return distance;
+		}
+		public static double Distance(Point A, Point B)
 		{
-			double x_dis = this.x - other.x;
-			double y_dis = this.y - other.y;
-			double distance = Math.Sqrt(Math.Pow(x_dis, 2) + Math.Pow(y_dis,2));
+			double x_dis = A.X - B.X;
+			double y_dis = A.Y - B.Y;
+			double distance = Math.Sqrt(Math.Pow(x_dis, 2) + Math.Pow(y_dis, 2));
 			return distance;
 		}
 		//public double X {  get; set; } //Автосвойства
 		//public double Y { get; set; }
+		public Point(double x = 0, double y = 0)
+		{
+			this.X = x;
+			this.Y = y;
+            Console.WriteLine("DefaultConstructor: " + this.GetHashCode());
+		}
+		public Point(Point other)
+		{
+			this.X = other.X;
+			this.Y = other.Y;
+            Console.WriteLine("CopyConstructor: " +this.GetHashCode());
+		}
+		~Point()
+		{
+            Console.WriteLine("Destructor:\t" + this.GetHashCode());
+		}
+		public static Point operator+(Point left, Point right)
+		{
+			Point res = new Point();
+			res.X = left.X + right.X;
+			res.Y = left.Y + right.Y;
+			return res;
+		}
+		public static Point operator-(Point left, Point right)
+		{
+			return new Point
+				(
+				left.X - right.X,
+				left.Y - right.Y
+				);
+		}
+		public static Point operator++(Point obj)
+		{
+			obj.X++;
+			obj.Y++;
+			return obj;
+		}
+		public static bool operator==(Point left, Point right)
+		{
+			return left.X == right.X && left.Y == right.Y;
+		}
+		public static bool operator!=(Point left, Point right)
+		{
+			return !(left == right);
+		}
 		public void Print()
 		{
 			Console.WriteLine("X = " + X + "\tY = " + Y);
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define DISTANCE 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +11,41 @@ namespace Encapsulation
 	internal class Program
 	{
 		static string delimeter = "\n|--------------------------------------|\n";
-		static double Distance(Point A, Point B)
-		{
-			double x_dis = A.X - B.X;
-			double y_dis = A.Y - B.Y;
-			double distance = Math.Sqrt(Math.Pow(x_dis, 2) + Math.Pow(y_dis, 2));
-			return distance;
-		}
+	
 		static void Main(string[] args)
 		{
+#if DISTANCE
 			Point A = new Point();
-			Point B = new Point();
+			Point C;
+			//Point B = new Point();
 			//Console.WriteLine("X = " + A.GetX() + "\tY = " + A.GetY());
 			//A.SetX(2000);
 			//A.SetY(3000);
 			//A.Print();
 			A.X = 1;
 			A.Y = 1;
-
+			Point B = new Point(A);
 			B.X = 2;
 			B.Y = 4;
 			A.Print();
-            Console.WriteLine();
+			Console.WriteLine();
 			B.Print();
-            Console.WriteLine(delimeter);
+			С = new Point(B);
+			Console.WriteLine(delimeter);
 			Console.WriteLine("Расстояние от точки 'B' до точки 'A': " + B.Distance(A));
-            Console.WriteLine(delimeter);
-            Console.WriteLine("Расстояние между точками 'A' и 'B': " + Distance(A, B));
+			Console.WriteLine(delimeter);
+			//Console.WriteLine("Расстояние между точками 'A' и 'B': " + Distance(A, B));  
+#endif
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(A + B);
+			C.Print();
+			Point D = new Point(A - B);
+			D.Print();
+			for(Point i = new Point(); i.X < 10; i++)
+			{
+				i.Print();
+			}
 		}
 	}
 }
